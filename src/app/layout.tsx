ec8114e517a5bb5517314fcbf1f8c77a88f7b849
Meraj Esmaeili <merajesmaeili492@gmail.com>
2026-07-24 22:10:47 +0330

    feat: add QueryProvider component and integrate React Query for data fetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { inter, quicksand, roboto } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import ApplyTheme from "@/components/behaviors/ApplyTheme";
 import { Figtree } from "next/font/google";
+import QueryProvider from "@/components/providers/QueryProvider";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
@@ -15,12 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("h-dvh", "antialiased", "font-sans", figtree.variable)}>
+    <html
+      lang="en"
+      className={cn("h-dvh", "antialiased", "font-sans", figtree.variable)}
+    >
       <ApplyTheme />
       <body
         className={`${quicksand.variable} ${roboto.variable} ${inter.variable}`}
       >
-        <div className="flex flex-col">{children}</div>
+        <QueryProvider>
+          <div className="flex flex-col">{children}</div>
+        </QueryProvider>
       </body>
     </html>
   );
