a4f2742ac0f752ea81e0383d9f39ac3836e19e19
Meraj Esmaeili <merajesmaeili492@gmail.com>
2026-08-05 01:15:01 +0330

    feat: enhance UserMenu component; add loading state with Shimmer and dynamic user display

--- src/components/shared/UserMenu.tsx
+++ src/components/shared/UserMenu.tsx
@@ -8,21 +8,28 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useAppSelector } from "@/lib/store/hooks";
 import { TbChevronDown, TbLogout, TbSettings, TbUser } from "react-icons/tb";
+import { Shimmer } from "@shimmer-from-structure/react";
 
 export function UserMenu() {
+  const user = useAppSelector((state) => state.user.data);
+  const isLoading = useAppSelector((state) => state.user.isLoading);
+
   const buttonClass =
     "flex cursor-pointer items-center justify-between my-1 gap-2 text-indigo-950 dark:text-indigo-50 hover:bg-black/20! dark:hover:bg-white/20! duration-100";
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button className="group flex items-center gap-2 rounded-md border px-3 py-1 text-sm outline-none">
-          <TbUser className="text-indigo-500" />
-          Meraj Esmaeili
-          <TbChevronDown className="text-indigo-500 transition-transform duration-200 group-data-[state=open]:rotate-180" />
-        </button>
-      </DropdownMenuTrigger>
+      <Shimmer loading={isLoading}>
+        <DropdownMenuTrigger asChild>
+          <button className="group flex items-center gap-2 rounded-md border px-3 py-1 text-sm outline-none">
+            <TbUser className="text-indigo-500" />
+            {user?.fullName ?? "Guest"}
+            <TbChevronDown className="text-indigo-500 transition-transform duration-200 group-data-[state=open]:rotate-180" />
+          </button>
+        </DropdownMenuTrigger>
+      </Shimmer>
 
       <DropdownMenuContent className="w-44" align="end">
         <DropdownMenuGroup>
